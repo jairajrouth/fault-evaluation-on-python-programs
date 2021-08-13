86acc21667caa475c0499dcfc2356d1f95577c4d
jairajrouth <jairajrouth@gmail.com>
2021-08-13 16:26:28 +0200

    Fixed the coverage matrix codes.

--- coverage_matrix.py
+++ coverage_matrix.py
@@ -4,7 +4,7 @@ from openpyxl import load_workbook
 
 
 def generate_line_coverage():
-    with open("buggy_version/htmlcov/black_py.html", "r") as f:
+    with open("buggy_coverage/black_py.html", "r") as f:
         statement_coverage_list = []
         for line in f.readlines():
             soup = BeautifulSoup(line, 'html.parser')
@@ -22,26 +22,27 @@ def generate_line_coverage():
                 statement_coverage_list.append(1)
 
         statement_coverage_df = pd.DataFrame(statement_coverage_list)
-        # print(statement_coverage_df)
-        cov_file_path = "buggy_coverage/statement_coverage_file.xlsx"
-        writer = pd.ExcelWriter(cov_file_path, engine='openpyxl', mode='a')
-        writer.book = load_workbook(cov_file_path)
-        writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
-        statement_coverage_df.to_excel(writer, index=None, header=None, startcol=96)
+        print(statement_coverage_df)
+        file_path = '/home/jai/fault-evaluation-on-python-programs/buggy_coverage/statement_coverage_file.xlsx'
+        book = load_workbook(file_path)
+        writer = pd.ExcelWriter(file_path, engine='openpyxl')
+        writer.book = book
+        writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+        statement_coverage_df.to_excel(writer, sheet_name="Sheet1", index=None, header=None, startcol=0, startrow=0)
         writer.save()
 
-
-def get_tests_result():
-    # Run py.test tests\test_black.py --excelreport=testreport2.xls --verbose
-    # for generating xlsx file.
-    data = pd.ExcelFile("buggy_coverage/test_report.xlsx").parse("Sheet1")
-    test_results_df = pd.DataFrame(data, columns=['RESULT'])
-    test_results_df = test_results_df.rename(columns={'RESULT': 'TEST_RESULT'})
-    test_results_df = test_results_df.T
-    test_results_df.to_excel("buggy_coverage/test_report_with_result_col.xlsx", startrow=0, startcol=0)
+# def get_tests_result():
+#     # Run py.test tests\test_black.py --excelreport=testreport2.xls --verbose
+#     # for generating xlsx file.
+#     data = pd.ExcelFile("buggy_coverage/test_report.xlsx").parse("Sheet1")
+#     test_results_df = pd.DataFrame(data, columns=['RESULT'])
+#     test_results_df = test_results_df.rename(columns={'RESULT': 'TEST_RESULT'})
+#     test_results_df = test_results_df.T
+#     test_results_df.to_excel("buggy_coverage/test_report_with_result_col.xlsx", startrow=0, startcol=0)
 
 
 if __name__ == '__main__':
-    # get_tests_result()
     generate_line_coverage()
+    # get_tests_result()
+
 
